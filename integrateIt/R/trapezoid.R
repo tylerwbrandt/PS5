c035deead1e695a03ba87300e018db44950516ea
Tyler Brandt <tylerbrandt02@gmail.com>
2018-03-01 00:33:42 -0600

    Begin trapezoid class creation
    Wrote R documentation for trapezoid class.

---
+++ integrateIt/R/trapezoid.R
@@ -0,0 +1,19 @@
+#' A trapezoid object
+#' 
+#' Object of class \code{trapezoid} are created by the \code{integrateIt} function
+#' 
+#' 
+#' An object of class 'trapezoid' has the following slots:
+#' \itemize{
+#' \item \code{height} the length of the subintervals
+#' \item \code{base1} f(x_(i-1))
+#' \item \code{base2} f(x_i)
+#' }
+#' 
+#' @author Tyler Brandt: \email{brandttyler@wustl.edu}
+#' @aliases trapezoid-class initialize
+#' @rdname trapezoid
+#' @export
+
+
+
