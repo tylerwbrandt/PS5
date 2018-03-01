abc96ab6e17264ee881eb925cb0a51b0d9a17cd1
Tyler Brandt <tylerbrandt02@gmail.com>
2018-03-01 14:19:57 -0600

    set up an s4 function
    Used setGeneric and setMethod to write integrateIt function.

---
+++ DevelopmentFile.R
@@ -0,0 +1,10 @@
+
+## Load libraries and set working directory
+library(devtools)
+library(roxygen2)
+setwd("~/Documents/Applied_Statistical_Programming/PS5/integrateIt") 
+
+## This is run once when the package strcuture is first created
+
+
+## At this point put the *.R files into the correcto directories and edit the DESCRIPTION file
--- integrateIt/R/integrateIt.R
+++ integrateIt/R/integrateIt.R
@@ -4,7 +4,7 @@
 #'
 #' @param x A numeric object
 #' @param y A numeric object with the same dimensionality as \code{x}.
-#' @param rule A string equal to either "Trap" or "Simpson"
+#' @param rule A character string equal to either "Trap" or "Simpson"
 #'
 #' @return An object of class Squares containing
 #'  \item{x}{The first object input}
@@ -14,9 +14,38 @@
 #' @note The estimate will depend on which method of integration you choose.
 #' @examples
 #' 
-#' myX <- c(20, 3) 
-#' myY <- c(-2, 4.1) 
-#' integrateIt(myX, myY)
+#' myX <- c(1, 2) 
+#' myY <- c(2, 4) 
+#' integrateIt(myX, myY, "Trap")
 #' @rdname integrateIt
 #' @aliases integrateIt
 #' @export
+setGeneric(name = "integrateIt",
+           def = function(x,y,rule,...){
+             standardGeneric("integrateIt")
+           })
+
+#' @export
+setMethod("integrateIt",
+          function(x, y, rule, ...){
+            a <- min(x)
+            b <- max(x)
+            h <- (b-a)/length(x)
+            x_sorted <- sort(x)
+            if (identical(rule, "Trap")){
+              area_sum <- sum(2*y)-a-b
+              area <- h*area_sum/2
+            } else if (identical(rule, "Simpson")){
+              area_sum <- 0
+              for (i in 1:length(y)){
+                if (i %% 2 == 0){
+                  area_sum <- area_sum + 4*y[i]
+                } else {
+                  area_sum <- area_sum + 2*y[i]
+                }
+              }
+              area_sum <- area_sum - a - b
+              area <- area_sum*h/3
+            }
+            return (new("trapezoid", x = x, y = y, area = area))
+          })
