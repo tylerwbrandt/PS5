0381a53640012962f38ed3ae5015b583e8f59c2f
Tyler Brandt <tylerbrandt02@gmail.com>
2018-03-06 01:26:31 -0600

    attempting to install package
    Updated development file to include the code to make it run in R and update NAMESPACE and DESCRIPTION and Rd files.

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -2,9 +2,14 @@
 ## Load libraries and set working directory
 library(devtools)
 library(roxygen2)
-setwd("~/Documents/Applied_Statistical_Programming/PS5/integrateIt") 
+setwd("~/Documents/Applied_Statistical_Programming/PS5") 
 
 ## This is run once when the package strcuture is first created
 
 
 ## At this point put the *.R files into the correcto directories and edit the DESCRIPTION file
+
+current.code <- as.package("integrateIt")
+load_all(current.code)
+document(current.code)
+test(current.code)
